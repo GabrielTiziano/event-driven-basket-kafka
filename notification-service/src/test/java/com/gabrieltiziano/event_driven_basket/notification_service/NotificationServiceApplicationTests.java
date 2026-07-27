ab47c23b58ec82dc5acc4e449103265431025728
GGhiaroni <gghiaronitiziano@gmail.com>
2026-07-26 21:45:40 -0300

    chore: estrutura inicial do monorepo com order-service e notification-service

---
+++ notification-service/src/test/java/com/gabrieltiziano/event_driven_basket/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrieltiziano.event_driven_basket.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ order-service/src/test/java/com/gabrieltiziano/event_driven_basket/order_service/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrieltiziano.event_driven_basket.order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
